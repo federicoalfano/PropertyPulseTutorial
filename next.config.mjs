9324f8b8a214e2aa5103acf0ceed7fd666fb9480
donky89 <donky1989@gmail.com>
2024-05-27 18:50:40 +0200

    some bugs to fix in auth. Developed session and profile image retrieval

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    images:
+    {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'ls3.googleusercontent.com',
+                pathname: '**'
+            }
+        ]
+    }
+};
 
 export default nextConfig;
--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,3 +1,5 @@
+import connectDB from '@/config/database';
+import User from '@/models/User';
 import GoogleProvider from 'next-auth/providers/google'
 
 
@@ -16,7 +18,25 @@ export const authOptions = {
         })
     ],
     callbacks: {
-        async signIn({profile}) {},
-        async session({session}) {}
+        async signIn({profile}) {
+            await connectDB();
+            const userExists = User.findOne({email: profile.email});
+            if(!userExists) {
+                const username = profile.name.slice(0, 20);
+                await User.create({
+                    email: profile.email,
+                    username,
+                    image: profile.picture
+                })
+            }
+            return true;
+        },
+        async session({session}) {
+
+            const user = await User.findOne({email: session.user.email});
+            session.user.id = user._id.toString()
+            return session;
+
+        }
     }
 }
